abc9442e8ac393398471b3a737be0c7b5996dc04
George Visconti Jr <georgevisconti00@gmail.com>
2022-10-08 12:06:10 -0400

    Solve 977A

---
+++ 977A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <vector>
+using namespace std;
+  
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+
+    for (int i = 0; i < k; i++)
+    {
+        if (n%10 == 0)
+        {
+            n /= 10;
+        } else {
+            n --;
+        }
+        
+    }
+    cout << n << endl;
+}
